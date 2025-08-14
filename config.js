90cb8606ad4089be788391e2ca3db1b325b311e8
wallyserviceleads <Wally@serviceleads.ai>
2025-08-14 06:39:55 -0500

    Update config.js
    updated the google sheet connection

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // === CONFIG ===
 // Option 1 (recommended): Publish your sheet to the web as CSV and paste the URL below.
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZuKt8DZm-1tRHa_yMxatO3SeI3mMvbE6Q2OZa9ogjFxs8HeKO57P1jrnK3snsniPbMtV4KSG1YK8y/pub?output=csv"; // e.g. https://docs.google.com/spreadsheets/d/1CG0WEyf-sEz8_3Y6jGzU0fZlEomb7w1P/export?format=csv&gid=0
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0SMcrtzhSiXYrdCnYX2Uk2I_bF4Uss2riC_8d6RkbAV1pU1G6FUTN0kWVnF5C7h1nmQiv06-h461Z/pubhtml"; // e.g. https://docs.google.com/spreadsheets/d/1CG0WEyf-sEz8_3Y6jGzU0fZlEomb7w1P/export?format=csv&gid=0
 
 // Option 2: If the sheet is shared publicly (anyone with link), you can often use export?format=csv directly:
 const SHEET_ID = "1CG0WEyf-sEz8_3Y6jGzU0fZlEomb7w1P"; // your Google Sheet ID
